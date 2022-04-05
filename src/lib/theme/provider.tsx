ca84cb54855f94b963e02c536e6012ad11cd4f30
Vinicius Pacheco <vin175pacheco@gmail.com>
2022-04-05 00:49:05 -0300

    feat(theme): make favicon swappable

--- src/lib/theme/provider.tsx
+++ src/lib/theme/provider.tsx
@@ -12,6 +12,11 @@ interface ThemeProviderProps {
   className?: string
 }
 
+const themeColor: Record<ThemeName, string> = {
+  dark: '#292524',
+  light: '#ffffff',
+}
+
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
   const [themeName, setTheme] = useState<ThemeName>('light')
   useEffect(() => {
@@ -41,14 +46,84 @@ export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
     [themeName, setThemeAndSavePreference],
   )
 
+  const favicon = (pathname: string): string =>
+    `/favicon/${themeName}${pathname}`
+
   return (
     <ThemeContext.Provider value={ctx}>
       <Head>
         <link
-          rel="icon"
-          href={themeName === 'dark' ? '/favicon.ico' : '/favicon-light.ico'}
-          type="image/x-icon"
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href={favicon('/apple-icon-57x57.png')}
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href={favicon('/apple-icon-60x60.png')}
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href={favicon('/apple-icon-72x72.png')}
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href={favicon('/apple-icon-76x76.png')}
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href={favicon('/apple-icon-114x114.png')}
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href={favicon('/apple-icon-120x120.png')}
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href={favicon('/apple-icon-144x144.png')}
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href={favicon('/apple-icon-152x152.png')}
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={favicon('/apple-icon-180x180.png')}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href={favicon('/android-icon-192x192.png')}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={favicon('/favicon-32x32.png')}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href={favicon('/favicon-96x96.png')}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={favicon('/favicon-16x16.png')}
+        />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="msapplication-TileColor" content={themeColor[themeName]} />
+        <meta name="theme-color" content={themeColor[themeName]} />
       </Head>
       {children}
     </ThemeContext.Provider>
