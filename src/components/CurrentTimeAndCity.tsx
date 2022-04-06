d7bd26033598a1fb3ae2a7b8d0baeeeac6e11862
Vinicius Pacheco <vin175pacheco@gmail.com>
2022-04-06 13:42:38 -0300

    fix: date causing html missmatch

--- src/components/CurrentTimeAndCity.tsx
+++ src/components/CurrentTimeAndCity.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FormattedMessage, useIntl } from 'react-intl'
 import cx from 'classnames'
 
@@ -10,19 +10,24 @@ const CurrentTimeAndCity: React.FC<Props> = ({ className }) => {
   const intl = useIntl()
 
   // Adjust time for GMT-3
-  const now = new Date()
-  now.setHours(now.getUTCHours() - 3)
+  const [now, setNow] = useState<Date | undefined>(undefined)
+
+  useEffect(() => {
+    const date = new Date()
+    date.setHours(date.getUTCHours() - 3)
+    setNow(date)
+  }, [])
 
   return (
     <div className={cx('', className)}>
       <h5 className="text-gray-400 uppercase text-xs font-semibold">
         <FormattedMessage id="Equvau" defaultMessage="Cidade atual" />
       </h5>
-      <p>
+      <p className={now ? undefined : 'blur'}>
         <FormattedMessage
           id="3kNnP9"
           defaultMessage="São Paulo, SP {time} 🇧🇷"
-          values={{ time: intl.formatTime(now) }}
+          values={{ time: now ? intl.formatTime(now) : '00' }}
         />
       </p>
     </div>
